063a05e9a87165ba7046a1235f83fd1dcc774e9a
Alan Garcia <agarcia13777@gmail.com>
2025-01-24 16:17:40 -0800

    Fixed some logic code for pulling 4 stars

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,12 @@ import { useState } from 'react'
 function App() {
 
   const [pulledResults, setPullResults] = useState([]);
-  const lastPityPullIndex = pulledResults.length - pulledResults.filter(pull => {
-    return pull.starRarity >= 4 ? pull : null;
-  }).length;
-  const last5PullIndex = pulledResults.length - pulledResults.filter(pull => {
-    return pull.starRarity < 5 ? pull : null;
-  }).length;
+  
+
   // const [last5PullIndex, setLast5PullIndex] = useState(0);
   // const [lastPityPullIndex, setLastPityPullIndex] = useState(0);
   // const [toggleCards, setToggleCards] = useState(false);
-  console.log(lastPityPullIndex, last5PullIndex);
+  //console.log(lastPityPullIndex, last5PullIndex);
   
   
   const pullElements = pulledResults.slice().reverse().map((resonator, index) => {
@@ -26,35 +22,76 @@ function App() {
   //   return <li key={index}>{`${pull.name != undefined ? pull.name : pull}  ${pulledResults.length - index}`}</li>
   // });
 
-  function simulatePull(){
+  function simulatePull(pullsToDo){
+    let newPulls = [];
+    
+    console.log(`${pulledResults.length}: Starting point`);
+
+    for(let i = 0; i < pullsToDo; i++){      
+      const [lastPityPullIndex, last5PullIndex] = findLastIndexOfPulls(pulledResults, newPulls);
+
+      console.log(lastPityPullIndex, last5PullIndex);
+  
       let fiveStarChance;
-      if((pulledResults.length - last5PullIndex) >= 65){
-        fiveStarChance = 0.008 + (pulledResults.length - last5PullIndex - 64) * 0.05;
+      if((pulledResults.length + newPulls.length - last5PullIndex) >= 65){
+        fiveStarChance = 0.008 + (pulledResults.length + newPulls.length  - last5PullIndex - 65) * 0.05;
       }else{
         fiveStarChance = 0.008;
       }
-
+  
       const fourStarChance = 0.06 + fiveStarChance;      
-
-      if((pulledResults.length - lastPityPullIndex) === 9){
-        setPullResults(prevResult => [...prevResult, getRandom4StarResonator()]);
-        // setLastPityPullIndex(pulledResults.length + 1);
-        return;
+  
+      if((pulledResults.length + newPulls.length - lastPityPullIndex) === 10){
+        newPulls.push(getRandom4StarResonator());
+        console.log('pity 4 pulled');
+        continue;
       }
       
       let randomChance = Math.random();      
-
+  
       if(randomChance < fiveStarChance){
-        setPullResults(prevResult => [...prevResult, getRandom5StarResonator()]);
-        // setLast5PullIndex(pulledResults.length + 1);
-        // setLastPityPullIndex(pulledResults.length + 1);
+        console.log('chance rate', randomChance);
+        console.log('five chance rate', fiveStarChance);
+        console.log('Since last 5 pity', pulledResults.length + newPulls.length - last5PullIndex);
+        newPulls.push(getRandom5StarResonator());
         console.log('5 Star Pulled');
       }else if(randomChance < fourStarChance){
-        setPullResults(prevResult => [...prevResult, getRandom4StarResonator()]);
-        // setLastPityPullIndex(pulledResults.length + 1);
+        console.log('chance rate', randomChance);
+        console.log('five chance rate', fiveStarChance);
+        newPulls.push(getRandom4StarResonator());
       }else{
-        setPullResults(prevResult => [...prevResult, {name: "3 Star", starRarity: 3, id: pulledResults.length}]);
+        newPulls.push({name: "3 Star", starRarity: 3, id: pulledResults.length});
       }
+    }    
+    setPullResults(prevResults => [...prevResults, ...newPulls]);
+  }
+
+  function findLastIndexOfPulls(pulledResults, newPulls){
+    const lastPityIndexPulled = pulledResults.findLastIndex((pull) =>    
+      pull.starRarity >= 4
+    ) === -1 ? 0 : pulledResults.findLastIndex((pull) =>    
+      pull.starRarity >= 4
+    ) ;
+
+    const last5PityIndexPulled = pulledResults.findLastIndex((pull) =>    
+      pull.starRarity === 5
+    ) === -1 ? 0 : pulledResults.findLastIndex((pull) =>    
+      pull.starRarity === 5
+    );
+
+    const lastPityIndexNew = newPulls.findLastIndex((pull) =>    
+      pull.starRarity >= 4
+    ) === -1 ? 0 : newPulls.findLastIndex((pull) =>    
+      pull.starRarity >= 4
+    );
+
+    const last5PityIndexNew = newPulls.findLastIndex((pull) =>    
+      pull.starRarity === 5
+    ) === -1 ? 0 : newPulls.findLastIndex((pull) =>    
+      pull.starRarity === 5
+    );
+
+    return [(lastPityIndexPulled + lastPityIndexNew), (last5PityIndexPulled + last5PityIndexNew)];
   }
 
   function getRandom4StarResonator(){
@@ -82,12 +119,8 @@ function App() {
         <h1 style={{marginTop:"10px"}}>Convene Pulling Simulator</h1>
         <img className='convene-art' src="/conveneSplashArt/roccia_convene_art.webp" alt="Splash Art for Convene" />
         <div className='convene-buttons-container'>
-          <button onClick={() => simulatePull()}>1 Convene</button>
-          <button onClick={() => {
-            for(let i = 0; i < 10; i++){
-              simulatePull();
-            }
-          }}>10 Convene</button>
+          <button onClick={() => simulatePull(1)}>1 Convene</button>
+          <button onClick={() => {simulatePull(10)}}>10 Convene</button>
         </div>
         
         <div className='resonator-card-container'>
--- src/components/ResonatorCard.jsx
+++ src/components/ResonatorCard.jsx
@@ -1,11 +1,13 @@
 import { clsx } from "clsx"
 
 export default function ResonatorCard(props){
-  const className = clsx("resonator-card", props.starRarity === 4 && "four-star-card", props.starRarity === 3 && "three-star-card", props.starRarity === 5 && "five-star-card")
+  const className = clsx("resonator-card", props.starRarity === 4 && "four-star-card", props.starRarity === 3 && "three-star-card", props.starRarity === 5 && "five-star-card");
+
+  const name = props.name === '3 Star' ? 'Eula' : props.name;
   return(
     <>
       <div className={className}>
-        <img src={`/resonatorPortraits/${props.name}_Card.webp`} alt={`Image of ${props.name}`} />
+        <img src={`/resonatorPortraits/${name}_Card.webp`} alt={`Image of ${props.name}`} />
         <div className="resonator-card-info">
           <h1>{props.name}</h1>
           <p>Star Rarity: {props.starRarity}</p>
